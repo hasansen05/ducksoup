31a52c1f0c5003b17295bdcd776c2aba051d1869
b0ykoe <b0ykoe@ymail.com>
2024-04-01 21:30:37 +0200

    debug: try catch all them party

--- DuckSoup/Agent/Vsro/PartyManagerHandlers.cs
+++ DuckSoup/Agent/Vsro/PartyManagerHandlers.cs
@@ -38,86 +38,101 @@ public class PartyManagerHandlers
 
     private async Task<Packet> PartyMatchingChangeResponse(SERVER_PARTY_MATCHING_CHANGE_RESPONSE data, ISession session)
     {
-        var entry = _partyManager.GetPartyMatchEntry((int)data.MatchingId);
+        try
+        {
+            var entry = _partyManager.GetPartyMatchEntry((int)data.MatchingId);
 
-        if (entry == null) return data;
+            if (entry == null) return data;
 
-        entry.Title = data.Title;
-        entry.PurposeType = data.partyPurpose;
-        entry.LevelMax = data.LevelRangeMax;
-        entry.LevelMin = data.LevelRangeMin;
+            entry.Title = data.Title;
+            entry.PurposeType = data.partyPurpose;
+            entry.LevelMax = data.LevelRangeMax;
+            entry.LevelMin = data.LevelRangeMin;
 
-        _partyManager.AddPartyMatchEntry(entry);
+            _partyManager.AddPartyMatchEntry(entry);
+        }
+        catch (Exception e)
+        {
+            Log.Error("{0}", e.InnerException);
+        }
 
         return data;
     }
 
     private async Task<Packet> PartyUpdate(SERVER_PARTY_UPDATE data, ISession session)
     {
-        ISession? sess = null;
-        switch (data.PartyUpdateType)
+        try
         {
-            case PartyUpdateType.Dismissed:
-                if (data.ErrorCode == PartyErrorCode.Dismissed)
-                {
-                    _partyManager.RemoveParty(session);
-                }
-
-                break;
-            case PartyUpdateType.Joined:
-                sess = await Helper.GetSessionByCharName(data.MemberInfo.Name);
-                if (sess == null) break;
-
-                sess.GetData(Data.CharInfo, out CharInfo? sessData, null);
-                if (sessData == null) break;
-                
-                var needsAdding = true;
-                var tParty = _partyManager.GetParty(session);
-                if (tParty == null)
-                {
-                    Log.Error("Couldn't find a party for the char {0}", sessData.CharName);
-                    break;
-                }
-                
-                foreach (var tPartyMember in tParty.Members)
-                {
-                    tPartyMember.GetData(Data.CharInfo, out CharInfo? tPartyData, null);
-                    if (tPartyData == null)
+            ISession? sess = null;
+            switch (data.PartyUpdateType)
+            {
+                case PartyUpdateType.Dismissed:
+                    if (data.ErrorCode == PartyErrorCode.Dismissed)
                     {
-                        continue;
+                        _partyManager.RemoveParty(session);
                     }
-                    
-                    if (tPartyData.Jid == sessData.Jid) needsAdding = false;
-                }
 
-                if (needsAdding) tParty.Members.Add(sess);
-
-                break;
-            case PartyUpdateType.Leave:
-                // everywhere
-                sess = await Helper.GetSessionByAccountJid((int)data.UserJID);
-                if (sess == null) break;
-
-                IParty? pt = _partyManager.GetParty(session);
-                if (pt == null || !pt.Members.Contains(sess))
-                {
                     break;
-                }
-                pt.Members.Remove(sess);
-                break;
-            case PartyUpdateType.Member:
-                break;
-            case PartyUpdateType.Leader:
-                sess = await Helper.GetSessionByAccountJid((int)data.UserJID);
-                if (sess == null) break;
+                case PartyUpdateType.Joined:
+                    sess = await Helper.GetSessionByCharName(data.MemberInfo.Name);
+                    if (sess == null) break;
 
-                var party = _partyManager.GetParty(session);
-                if (party == null) break;
+                    sess.GetData(Data.CharInfo, out CharInfo? sessData, null);
+                    if (sessData == null) break;
 
-                party.Leader = sess;
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
+                    var needsAdding = true;
+                    var tParty = _partyManager.GetParty(session);
+                    if (tParty == null)
+                    {
+                        Log.Error("Couldn't find a party for the char {0}", sessData.CharName);
+                        break;
+                    }
+
+                    foreach (var tPartyMember in tParty.Members)
+                    {
+                        tPartyMember.GetData(Data.CharInfo, out CharInfo? tPartyData, null);
+                        if (tPartyData == null)
+                        {
+                            continue;
+                        }
+
+                        if (tPartyData.Jid == sessData.Jid) needsAdding = false;
+                    }
+
+                    if (needsAdding) tParty.Members.Add(sess);
+
+                    break;
+                case PartyUpdateType.Leave:
+                    // everywhere
+                    sess = await Helper.GetSessionByAccountJid((int)data.UserJID);
+                    if (sess == null) break;
+
+                    IParty? pt = _partyManager.GetParty(session);
+                    if (pt == null || !pt.Members.Contains(sess))
+                    {
+                        break;
+                    }
+
+                    pt.Members.Remove(sess);
+                    break;
+                case PartyUpdateType.Member:
+                    break;
+                case PartyUpdateType.Leader:
+                    sess = await Helper.GetSessionByAccountJid((int)data.UserJID);
+                    if (sess == null) break;
+
+                    var party = _partyManager.GetParty(session);
+                    if (party == null) break;
+
+                    party.Leader = sess;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error("{0}", e.InnerException);
         }
 
         return data;
@@ -125,94 +140,118 @@ public class PartyManagerHandlers
 
     private async Task<Packet> PartyMatchingDeleteResponse(SERVER_PARTY_MATCHING_DELETE_RESPONSE data, ISession session)
     {
-        _partyManager.RemovePartyMatchEntry((int)data.MatchingId);
+        try
+        {
+            _partyManager.RemovePartyMatchEntry((int)data.MatchingId);
+        }
+        catch (Exception e)
+        {
+            Log.Error("{0}", e.InnerException);
+        }
+
         return data;
     }
 
     private async Task<Packet> PartyMatchingFormResponse(SERVER_PARTY_MATCHING_FORM_RESPONSE data, ISession session)
     {
-        IParty? party;
-        if (data.Id == 0)
-            party = new Party
+        try
+        {
+            IParty? party;
+            if (data.Id == 0)
+                party = new Party
+                {
+                    PartyId = (int)data.Id,
+                    Leader = session,
+                    Members = new List<ISession> { session },
+                    PartySettingsFlag = data.partySetting
+                };
+            else
             {
-                PartyId = (int)data.Id,
-                Leader = session,
-                Members = new List<ISession> { session },
-                PartySettingsFlag = data.partySetting
+                party = _partyManager.GetParty((int)data.Id);
+            }
+
+            if (party == null)
+            {
+                return data;
+            }
+
+            IPartyMatchEntry? partyMatchEntry = new PartyMatchEntry
+            {
+                LevelMax = data.LevelRangeMax,
+                LevelMin = data.LevelRangeMin,
+                MatchId = (int)data.MatchingId,
+                Party = party,
+                PurposeType = data.partyPurpose,
+                Title = data.Title
             };
-        else
-        {
-            party = _partyManager.GetParty((int)data.Id);
+
+            _partyManager.AddPartyMatchEntry(partyMatchEntry);
         }
-
-        if (party == null)
+        catch (Exception e)
         {
-            return data;
+            Log.Error("{0}", e.InnerException);
         }
-
-        IPartyMatchEntry? partyMatchEntry = new PartyMatchEntry
-        {
-            LevelMax = data.LevelRangeMax,
-            LevelMin = data.LevelRangeMin,
-            MatchId = (int)data.MatchingId,
-            Party = party,
-            PurposeType = data.partyPurpose,
-            Title = data.Title
-        };
-
-        _partyManager.AddPartyMatchEntry(partyMatchEntry);
 
         return data;
     }
 
     private async Task<Packet> PartyCreateFromMatching(SERVER_PARTY_CREATE_FROM_MATCHING data, ISession session)
     {
-        var party = _partyManager.GetParty(data.ID);
-        if (party != null || data.ID == 0) return data;
+        try
+        {
+            var party = _partyManager.GetParty(data.ID);
+            if (party != null || data.ID == 0) return data;
 
-        var leaderSession = await Helper.GetSessionByAccountJid(data.LeaderJID);
-        if (leaderSession == null)
-        {
-            return data;
-        }
-        
-        party = new Party
-        {
-            Leader = leaderSession,
-            PartyId = data.ID,
-            PartySettingsFlag = data.PartySettingsFlag
-        };
-
-        foreach (var dataMemberInfo in data.MemberInfos)
-        {
-            var sess = await Helper.GetSessionByAccountJid(dataMemberInfo.JID);
-            if (sess == null)
+            var leaderSession = await Helper.GetSessionByAccountJid(data.LeaderJID);
+            if (leaderSession == null)
             {
-                continue;
+                return data;
             }
-            party.Members.Add(sess);
-        }
-        _partyManager.AddParty(party);
 
-        var partyMatchEntry = _partyManager.GetPartyMatchEntries()
-            .FirstOrDefault(entry =>
+            party = new Party
             {
-                if (entry == null || entry.Party == null)
+                Leader = leaderSession,
+                PartyId = data.ID,
+                PartySettingsFlag = data.PartySettingsFlag
+            };
+
+            foreach (var dataMemberInfo in data.MemberInfos)
+            {
+                var sess = await Helper.GetSessionByAccountJid(dataMemberInfo.JID);
+                if (sess == null)
                 {
-                    return false;
+                    continue;
                 }
-                
-                entry.Party.Leader.GetData(Data.CharInfo, out CharInfo? entryInfo, null);
-                leaderSession.GetData(Data.CharInfo, out CharInfo? leaderInfo, null);
-                if (entryInfo == null || leaderInfo == null)
+
+                party.Members.Add(sess);
+            }
+
+            _partyManager.AddParty(party);
+
+            var partyMatchEntry = _partyManager.GetPartyMatchEntries()
+                .FirstOrDefault(entry =>
                 {
-                    return false;
-                }
-                
-                return leaderInfo.Jid == entryInfo.Jid;
-            });
-        
-        if (partyMatchEntry != null) partyMatchEntry.Party = party;
+                    if (entry == null || entry.Party == null)
+                    {
+                        return false;
+                    }
+
+                    entry.Party.Leader.GetData(Data.CharInfo, out CharInfo? entryInfo, null);
+                    leaderSession.GetData(Data.CharInfo, out CharInfo? leaderInfo, null);
+                    if (entryInfo == null || leaderInfo == null)
+                    {
+                        return false;
+                    }
+
+                    return leaderInfo.Jid == entryInfo.Jid;
+                });
+
+            if (partyMatchEntry != null) partyMatchEntry.Party = party;
+        }
+        catch (Exception e)
+        {
+            Log.Error("{0}", e.InnerException);
+        }
 
         return data;
     }
