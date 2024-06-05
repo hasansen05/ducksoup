eb94d8f1fc36cd33359a83759420ee55e94d4286
b0ykoe <b0ykoe@ymail.com>
2024-06-05 14:49:50 +0200

    fix: fix 0x7021, thanks to Monterato

--- PacketLibrary/VSRO188/Agent/Client/CLIENT_ENTITY_MOVEMENT_REQUEST.cs
+++ PacketLibrary/VSRO188/Agent/Client/CLIENT_ENTITY_MOVEMENT_REQUEST.cs
@@ -6,7 +6,9 @@ namespace PacketLibrary.VSRO188.Agent.Client;
 // https://github.com/DummkopfOfHachtenduden/SilkroadDoc/wiki/AGENT_ENTITY_MOVEMENT
 public class CLIENT_ENTITY_MOVEMENT_REQUEST : Packet
 {
-    public byte UnkByte0;
+    public byte ClickType; // 0 = sky/keys, 1 = ground
+    public byte Unk1;
+    public short Unk2;
     public Position Position;
     
     public CLIENT_ENTITY_MOVEMENT_REQUEST() : base(0x7021)
@@ -20,15 +22,33 @@ public class CLIENT_ENTITY_MOVEMENT_REQUEST : Packet
 
     public override async Task Read()
     {
-        TryRead(out UnkByte0);
-        Position = Position.FromPacketConditional(this, false);
+        TryRead<byte>(out ClickType);
+        switch (ClickType)
+        {
+            case 0:
+                TryRead<byte>(out Unk1);
+                TryRead<short>(out Unk2);
+                break;
+            case 1:
+                Position = Position.FromPacketConditional(this, false);
+                break;
+        }
     }
 
     public override async Task<Packet> Build()
     {
         Reset();
-        TryWrite(UnkByte0);
-        Position.ToPacketConditional(this, false);
+        TryWrite<byte>(ClickType);
+        switch (ClickType)
+        {
+            case 0:
+                TryWrite<byte>(Unk1);
+                TryWrite<short>(Unk2);
+                break;
+            case 1:
+                Position.ToPacketConditional(this, false);
+                break;
+        }
         return this;
     }
 
